70d7be543e7c53f5787fde53e5c0cacfe43f1d5d
AnthSuz <mil4@hotmail.fr>
2019-12-20 23:43:05 +0100

    test link logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import Logo from "../images/logo.jpg";
 
 const Header = () => {
@@ -7,7 +7,9 @@ const Header = () => {
     <>
       <div className="header">
         <div className="wrapper">
-          <img src={Logo} alt="Logo MeilleurTaux.com" className="Logo" />
+          <a href="https://fake-meilleurtaux.netlify.com/">
+            <img src={Logo} alt="Logo MeilleurTaux.com" className="Logo" />
+          </a>
           <p>Crédit immobilier : 5 mn pour obtenir le meilleur taux</p>
         </div>
       </div>
